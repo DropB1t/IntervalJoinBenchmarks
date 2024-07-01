39b7f746c857e1cb56be632e87adeb261e44f100
Yuriy Rymarchuk <yura.ita.com@gmail.com>
2024-07-01 18:53:57 +0200

    [FIX] reset config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -5,14 +5,14 @@ SAMPLING=250
 
 parallelism=( 1 2 4 6 8 16 32 )
 source_degrees=( 1 2 3 4 )
-num_key=( 10000 )
-batch_size=( 16 )
+num_key=( 1000 10000 )
+batch_size=( 0 16 32 )
 
 lower_bounds=( -500 )
 upper_bounds=( 500 )
 
 exec_mode=( k d )
-real_type=( )
+real_type=( rd sd )
 
 synt_type=( su sz )
-zipfian_skews=( 0.0 )
+zipfian_skews=( 0.0 0.9 )
